0e041b87b777e4a3da0ba96eabb01469afcdc725
Daniel Vyenielo <dvyenielo@gmail.com>
2020-03-23 17:37:29 -0700

    Refine and simplify interface for computation of Julian Date.

--- julian_date.h
+++ julian_date.h
@@ -2,13 +2,6 @@
 #define SUNRISE_SUNSET_JULIAN_DAY
 #include <cstdint>
 
-int_fast32_t julian_day_number(int_fast16_t const year,
-                               int_fast8_t const month,
-                               int_fast8_t const day);
-double julian_date(int_fast32_t julian_day_number,
-                   int_fast8_t const hour,
-                   int_fast8_t const minute,
-                   int_fast8_t const second);
 double julian_date(int_fast16_t const year,
                    int_fast8_t const month,
                    int_fast8_t const day,
--- test_julian_date.cpp
+++ test_julian_date.cpp
@@ -1,28 +1,74 @@
 #include "julian_date.h"
+#include <cmath>
 #include <cassert>
+#include <iostream>
+
+constexpr double ABSOLUTE_ERROR_THRESHOLD{ 0.000'1 };
+
+double absolute_error(double const lhs, double const rhs)
+{
+    return std::abs(lhs - rhs);
+}
+
+bool test_aavso_example(int_fast16_t const year,
+                        int_fast8_t const month,
+                        int_fast8_t const day,
+                        int_fast8_t const hour, 
+                        int_fast8_t const minute, 
+                        int_fast8_t const second,
+                        double const expected_jd)
+{
+    double actual_jd{ julian_date(year, 
+                      month, 
+                      day, 
+                      hour, 
+                      minute, 
+                      second) };
+    double abs_err{ absolute_error(actual_jd, 
+                                   expected_jd) };
+    printf("absolute_error(%f, %f) == %f\n", actual_jd, expected_jd, abs_err);
+    return abs_err < ABSOLUTE_ERROR_THRESHOLD;
+}
 
 bool test_aavso_example_1()
-// Returns true if actual and expected Julian Day Numbers (jdn) are equal.
+// Returns true if test passes otherwise returns false.
 // https://www.aavso.org/computing-jd
 {
     int_fast16_t year{ 2010 };
     int_fast8_t month{ 1 };
     int_fast8_t day{ 9 };
-    int_fast32_t actual_jdn{ julian_day_number(year, month, day) };
+    int_fast8_t hour{ 23 };
+    int_fast8_t minute{ 15 };
+    int_fast8_t second{ 0 };
     int_fast32_t expected_jdn{ 2'455'206 };
-    return actual_jdn == expected_jdn;
+    double expected_jd{ 2'455'206.468'8 };
+    return test_aavso_example(year,
+                              month,
+                              day,
+                              hour,
+                              minute,
+                              second,
+                              expected_jd);
 }
 
 bool test_aavso_example_2()
-// Returns true if actual and expected Julian Day Numbers (jdn) are equal.
+// Returns true if test passes otherwise returns false.
 // https://www.aavso.org/computing-jd
 {
     int_fast16_t year{ 2010 };
     int_fast8_t month{ 2 };
     int_fast8_t day{ 14 };
-    int_fast32_t actual_jdn{ julian_day_number(year, month, day) };
-    int_fast32_t expected_jdn{ 2'455'242 };
-    return actual_jdn == expected_jdn;
+    int_fast8_t hour{ 13 };
+    int_fast8_t minute{ 21 };
+    int_fast8_t second{ 0 };
+    double expected_jd{ 2'455'242.056'3 };
+    return test_aavso_example(year,
+                              month,
+                              day,
+                              hour,
+                              minute,
+                              second,
+                              expected_jd);
 }
 
 bool test_aavso_example_3()
@@ -31,10 +77,36 @@ bool test_aavso_example_3()
 {
     int_fast16_t year{ 2010 };
     int_fast8_t month{ 1 };
-    int_fast8_t day{ 27 };
-    int_fast32_t actual_jdn{ julian_day_number(year, month, day) };
-    int_fast32_t expected_jdn{ 2'455'224 };
-    return actual_jdn == expected_jdn;
+    int_fast8_t day{ 28 };
+    int_fast8_t hour{ 9 };
+    int_fast8_t minute{ 25 };
+    int_fast8_t second{ 0 };
+    double expected_jd{ 2'455'224.892'4 };
+    return test_aavso_example(year,
+                              month,
+                              day,
+                              hour,
+                              minute,
+                              second,
+                              expected_jd);
+}
+
+bool test_recipes_example_today()
+{
+    int_fast16_t year{ 2020 };
+    int_fast8_t month{ 3 };
+    int_fast8_t day{ 23 };
+    int_fast8_t hour{ 12 };
+    int_fast8_t minute{ 0 };
+    int_fast8_t second{ 0 };
+    double expected_jd{ 2'458'932.5 };
+    return test_aavso_example(year,
+                              month,
+                              day,
+                              hour,
+                              minute,
+                              second,
+                              expected_jd);
 }
 
 int main()
@@ -42,4 +114,5 @@ int main()
     assert(test_aavso_example_1());
     assert(test_aavso_example_2());
     assert(test_aavso_example_3());
+    assert(test_recipes_example_today());
 }
